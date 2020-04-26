f73bf24cc75d2a9b71bd295e631f269de599a7d0
jinah <jinah0818@gmail.com>
2020-04-26 22:33:37 +0900

    Practice Counter App

---
+++ ClassCounter/src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Styled from 'styled-components/native';
+import Counter from './Screens/Counter';
+
+const Container = Styled.View`flex: 1; background-color: #EEE`;
+
+const App = () => {
+  return (
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
+  );
+};
+
+export default App;
