246caf44f2c0587ec8842dc00a341f99f08ea233
jinah <jinah0818@gmail.com>
2020-04-28 00:20:05 +0900

    Add Components 1

---
+++ TodoList/src/Screen/Todo/TodoListView/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`height:40px;justify-content:center;align-items:center;`;
+const TitleLabel = Styled.View`font-size:24px;font-weight:bold;`;
+
+interface Props {}
+
+const Header = ({}: Props) => {
+  return (
+    <Container>
+      <TitleLabel>Todo List App</TitleLabel>
+    </Container>
+  );
+};
+
+export default Header;
