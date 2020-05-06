968ce81b616e560d34bcacd695e2d0faa66fe06e
jinah <jinah0818@gmail.com>
2020-05-07 01:15:12 +0900

    Update components(Login/CheckLogin/Input/Button)

--- MovieApp/src/App.tsx
+++ MovieApp/src/App.tsx
@@ -5,37 +5,17 @@
  * @format
  * @flow strict-local
  */
-
-import React, {Fragment, useEffect} from 'react';
-import {StatusBar, SafeAreaView, Text} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import Styled from 'styled-components/native';
-import SplashScreen from 'react-native-splash-screen';
-
-const ScrollView = Styled.SafeAreaView`background-color: ${Colors.lighter};`;
-const Body = Styled.View`background-color: ${Colors.white};`;
-const SectionContainer = Styled.View`margin-top:32px;padding-horizontal:24px;`;
-const SectionDescriptioin = Styled.Text`margin-top:8px; font-size: 18px; font-weight: 400; color: ${Colors.dark};`;
-const HighLight = Styled.Text`font-weight: 700;`;
-
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Navigator from '~/Screens/Navigator';
 interface Props {}
 
 const App = ({}: Props) => {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
   return (
-    <Fragment>
-      <Text>Hello</Text>
-    </Fragment>
+    <>
+      <StatusBar barStyle="light-content" />
+      <Navigator />
+    </>
   );
 };
 
---
+++ MovieApp/src/Screens/CheckLogin/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {NavigationScreenProp, NavigationState} from 'react-navigation';
+import {ActivityIndicator} from 'react-native';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`flex: 1; background-color: #141414; justify-content: center; align-items: center;`;
+
+interface Props {
+  navigation: NavigationScreenProp<NavigationState>;
+}
+
+const CheckLogin = ({navigation}: Props) => {
+  AsyncStorage.getItem('key')
+    .then((value) => {
+      if (value) {
+        navigation.navigate('MovieNavigator');
+      } else {
+        navigation.navigate('LoginNavigator');
+      }
+    })
+    .catch((error: Error) => {
+      console.log(error);
+    });
+
+  return (
+    <Container>
+      <ActivityIndicator size="large" color="#E70915" />
+    </Container>
+  );
+};
+
+CheckLogin.navigationOptioins = {header: null};
+
+export default CheckLogin;
